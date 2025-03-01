c093e1b3d55dff585d00a91424494d07e856ec4b
Donna Ha <65718414+Donna-Smee@users.noreply.github.com>
2025-02-28 21:42:42 -0500

    feat: added Prometheus metrics endpoint and error endpoint in web app

--- app.js
+++ app.js
@@ -3,11 +3,39 @@ const promClient = require('prom-client');
 const app = express();
 const PORT = 3000; // put this as variable later maybe
 
+// Create Prometheus metrics
+const httpRequestDurationMicroseconds = new promClient.Counter({
+    name: 'http_request_duration_seconds',
+    help: 'Duration of HTTP requests in seconds',
+    labelNames: ['method', 'status'],
+  });
+  
+  // Expose metrics endpoint for Prometheus
+  app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', promClient.register.contentType);
+    res.end(await promClient.register.metrics());
+  });
 
 // Simple hello world endpoint
 app.get("/", (req, res, next) => {
     res.sendFile(__dirname + '/public/index.html');
 });
+
+// Error route for testing error cases (500 errors)
+app.get('/error', (req, res) => {
+    res.status(500).send('Server Error');
+  });
+  
+  // Middleware to track errors
+  app.use((err, req, res, next) => {
+    if (err) {
+      httpRequestDurationMicroseconds.inc({
+        method: req.method,
+        status: '500',
+      });
+    }
+    next(err);
+  });
 
 app.listen(PORT, () => {
     console.log(`Web app is running on http://localhost:${PORT}`);
